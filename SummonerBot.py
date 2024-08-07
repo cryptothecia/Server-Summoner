d22225cd526103a03ff93b333066c63094830d0f
cryptothecia <cretaceousbob@gmail.com>
2024-08-06 22:08:30 -0400

    Added logging messages to logfile if enabled from .env

--- SummonerBot.py
+++ SummonerBot.py
@@ -3,13 +3,25 @@ import os
 import discord
 import socket
 import struct
-
+import datetime
 from discord import app_commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 currentGamePath = os.getenv('currentGamePath')
+logging = (os.getenv('logging')) == "True"
+time = datetime.datetime.now()
+logFile=os.path.join((os.path.abspath(__file__)).replace(os.path.basename(__file__),""),"summonerlog.txt")
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
+games = {
+    "Palworld" : "Palworld",
+    "7D2D" : "7 Days to Die",
+    "something" : "something"
+}
 
 #This section builds information for sending magic packets
 MAC = os.getenv('MAC')
@@ -24,25 +36,23 @@ for i in range(0, len(MACBytes), 2):
     ])
 #End magic packet build
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = discord.Client(intents=intents)
-tree = app_commands.CommandTree(bot)
-
-games = {
-    "Palworld" : "Palworld",
-    "7D2D" : "7 Days to Die",
-    "something" : "something"
-}
+def log(logMessage):
+    if logging is True:
+        if (os.path.exists(logFile)) is False:
+            with open(logFile, "w") as f:
+                f.write("")
+        with open(logFile, "a") as f:
+            f.write(time.strftime("%Y/%m/%d_%H:%M:%S") + ":: " + logMessage + "\n")
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    log("")
+    log(f'{bot.user} has connected to Discord!')
     await tree.sync() 
 
 async def summon(summonedGame,interaction):
     await interaction.response.defer(ephemeral=True,thinking=True)
+    log(f"{interaction.user} used {interaction.command.name} in {interaction.channel}")
     if (os.path.exists(currentGamePath)) is False:
         with open(currentGamePath, "w") as f:
             f.write("")
@@ -57,14 +67,16 @@ async def summon(summonedGame,interaction):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as netConnect:
             netConnect.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             netConnect.sendto(MagicPacket, ("255.255.255.255",7))
-        await interaction.followup.send(f"Bringing {games[summonedGame]} server online.",ephemeral=True)
+        message = f"Bringing {games[summonedGame]} server online."
     elif "Locked" in currentGame:
         currentGame = currentGame.replace("Locked","").replace('\n',"")
         if currentGame is None:
             currentGame = "something"
-        await interaction.followup.send(f"Looks like the dedicated server should already be online running {games[currentGame]}.",ephemeral=True)
+        message = f"Looks like the dedicated server should already be online running {games[currentGame]}."
     else:
-        await interaction.followup.send("Oh dear, something went wrong. Sorry.",ephemeral=True)   
+        message = "Oh dear, something went wrong. Sorry."
+    await interaction.followup.send(message,ephemeral=True)
+    log(f"Sent to {interaction.user}: \"" + message + "\"")
 
 @tree.command(name="summonpalworld",description=f"Send a request to bring the {games[list(games.keys())[0]]} dedicated server online.")
 async def summonpalworld(interaction: discord.Interaction):
