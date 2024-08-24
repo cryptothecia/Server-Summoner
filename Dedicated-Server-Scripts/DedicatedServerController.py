ba57dbfb3fb5b1416a23a575d01312ebcaa10233
cryptothecia <cretaceousbob@gmail.com>
2024-08-23 20:39:17 -0400

    Added create_game_services

--- Dedicated-Server-Scripts/DedicatedServerController.py
+++ Dedicated-Server-Scripts/DedicatedServerController.py
@@ -1,6 +1,7 @@
 import socket
 import subprocess
 import glob
+import os
 
 # Build list of games that the dedicated server can run
 servicePath = '/etc/systemd/system/*Server.service'
@@ -18,19 +19,39 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen(1)
 
+### Assuming any Server.service exists, creates new Server and Backup .services for the requested game  
+def create_game_services(game: str):
+    path = servicePath.replace('*',games[0])
+    with open(path,"r") as f:
+        extantServerService=(f.read()).replace(games[0],game).replace("Description=*\n",f"Description={game} server.\n")
+    with open(path.replace("Server.service","Backup.service"),"r") as f:
+        extantBackupService=(f.read()).replace(games[0],game).replace("Description=*\n",f"Description=Service for backing up {game} server.\n")
+    path = path.replace(games[0],game)
+    with open(path,"w") as f:
+        f.write(extantServerService)
+    with open(path.replace("Server.service","Backup.service"),"w") as f:
+        f.write(extantBackupService)   
+    subprocess.run(['sudo','systemctl','daemon-reload', f'{game}Server.service'], text=True)
+
+### Invokes create_game_services if there are no services for the requested game, then checks the status of the services    
 def check_game_services(game: str,backupRequested: bool):
+    path = servicePath.replace('*',game)
+    if (os.path.isfile(path)) is False:
+        create_game_services(game)
     serverStatus = subprocess.run(['systemctl', 'is-active', f'{game}Server.service'], text=True, stdout=subprocess.PIPE)
     if backupRequested == True:
         backupStatus = subprocess.run(['systemctl', 'is-active', f'{game}Backup.service'], text=True, stdout=subprocess.PIPE)
         return serverStatus.stdout.strip(), backupStatus.stdout.strip()
     else: 
         return serverStatus.stdout.strip()
-    
+ 
+### Starts the services for the requested game 
 def start_game_services(game: str,backupRequested: bool):
     subprocess.run(['sudo', 'systemctl', 'start', f'{game}Server.service'], text=True)
     if backupRequested == True:
         subprocess.run(['sudo', 'systemctl', 'start', f'{game}Backup.service'], text=True)
 
+### Returns what game(s) has services currently running
 def get_current_game():
     ag = []
     for game in games:
@@ -43,6 +64,7 @@ def get_current_game():
     else:
         return ag
 
+### Invokes functions in response to requests from SummonerBot.py, always returning a string as an answer
 def reply(request):
     match request:
         case "status":
@@ -70,6 +92,7 @@ def reply(request):
         case _:
             return "No request made"
 
+### Loop for socket to listen for and send responses to requests from SummonerBot.py
 conn, addr = s.accept()
 while True:
     request = conn.recv(buffer_size).decode()
