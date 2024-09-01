a618c4844dc64606ae19a274891fee27e338fc7d
cryptothecia <cretaceousbob@gmail.com>
2024-09-01 01:11:19 -0400

    Added SteamCMD update script

--- Dedicated-Server-Scripts/Servers.sh
+++ Dedicated-Server-Scripts/Servers.sh
@@ -10,20 +10,54 @@ do
 done
 
 serverLocation=_${game}_ServerLocation
+serverLocation="${!serverLocation}"
 
-if [ -f "${!serverLocation}" ]; then
+if [ -f "$serverLocation" ]; then
+    ### Gets server directory by searching for index of last slash
+    slashIndex=$(echo "$serverLocation" | grep -ob "/" | tail -n 1 | tr -d :/)
+    serverFolder=${serverLocation:0:(slashIndex + 1)}
+    ### Looks for a SteamCMD update script to run (main purpose of script would be to update the server)
+    updateScript=$serverFolder"update_${game}.txt"
+    if [ -f "$updateScript" ]; then
+        steamcmd +runscript "$updateScript"
+    ### If a script isn't found, one is created
+    else
+        ### Gets the index of "steamapps" in the serverFolder, if there is no steamapps folder in the path, the script will not be generated
+        steamappsIndex=$(echo "$serverFolder" | grep -ob "steamapps" | tr -d ":stemap")
+        steamappsFolder=${serverLocation:0:(steamappsIndex + 9)}
+        ### If steamapps folder is valid directory, the for loop will read every appmanifest file to find one that has an installdir that matches the game being launched
+        if [ -d "$steamappsFolder" ]; then
+            for file in "$steamappsFolder"/appmanifest*; do
+                findInstallDir=$(grep "installdir" "$file" | tr -d "\"")
+                installDir="${findInstallDir:13:${#findInstallDir}}"
+                ### If installdir is found that matches part of the path in serverLocation, then that file is searched for an appID, which is then used to create a script
+                if [[ $serverFolder == *$installDir* ]]; then
+                    findAppID=$(grep "appid" "$file" | tr -d "\"")
+                    appID="${findAppID:8:${#findAppID}}"
+                    if [[ ! -z "$appID" ]]; then 
+                        updateScriptTemplate=$(cat ./update_game_template.txt)
+                        updateScriptBody=${updateScriptTemplate//appid/$appID}
+                        updateScriptBody=${updateScriptBody//steam-library/$steamappsFolder}
+                        updateScriptBody=${updateScriptBody//steamapps/}
+                        echo "$updateScriptBody" > "$updateScript"
+                        break
+                    fi
+                fi
+            done
+        fi
+    fi
     case "${game}" in
         "Palworld") 
-            "${!serverLocation}" -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS      echo "ran"
+            "$serverLocation" -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS      echo "ran"
         ;;
         "7D2D")
-            "${!serverLocation}" -configfile="${_7D2D_ServerConfig}"
+            "$serverLocation" -configfile="${_7D2D_ServerConfig}"
         ;;
         "Enshrouded")
-            wine64 "${!serverLocation}"
+            wine64 "$serverLocation"
         ;;
         *)
-            "${!serverLocation}"
+            "$serverLocation"
     esac
 else
     echo "Server location isn't valid."
