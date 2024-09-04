c913128d0b4c4544f885e10aa7f15d3abd32b2e4
cryptothecia <cretaceousbob@gmail.com>
2024-09-03 20:17:10 -0400

    Reworked creating and running steamCMD update script

--- Dedicated-Server-Scripts/Servers.sh
+++ Dedicated-Server-Scripts/Servers.sh
@@ -13,36 +13,45 @@ serverLocation=_${game}_ServerLocation
 serverLocation="${!serverLocation}"
 
 if [[ -f $serverLocation ]]; then
-    ### Gets server directory by searching for index of last slash
-    slashIndex=$(echo "$serverLocation" | grep -ob "/" | tail -n 1 | tr -d :/)
-    serverFolder=${serverLocation:0:(slashIndex + 1)}
-    steamappsFolder="$serverFolder"steamapps
-    while [[ ! -d $steamappsFolder ]]; do
-        serverFolder=${serverFolder:0:(${#serverFolder}-1)}
-        slashIndex=$(echo "$serverFolder" | grep -ob "/" | tail -n 1 | tr -d :/)
-        serverFolder=${serverFolder:0:(slashIndex + 1)}
-        steamappsFolder="$serverFolder"steamapps
-    done
     ### Checks if steamcmd is a valid command
     steamCMDcheck=$(command -v steamcmd)
-    ### Looks for a SteamCMD update script to run (main purpose of script would be to update the server)
-    updateScript=$serverFolder"update_${game}.txt"
-    if [[ -f "$updateScript" && -n $steamCMDcheck ]]; then
-        steamcmd +runscript "$updateScript"
-    ### If a script isn't found, one is created
-    elif [[ ! -f "$updateScript" && -n $steamCMDcheck ]]; then
+    if [[ -z $steamCMDcheck ]]; then
+        echo "SteamCMD is not installed."
+    elif [[ -n $steamCMDcheck ]]; then
+        ### Gets server directory and steamapps folder by searching for index of last slash, then tries to find appID
+        slashIndex=$(echo "$serverLocation" | grep -ob "/" | tail -n 1 | tr -d :/)
+        serverFolder=${serverLocation:0:(slashIndex + 1)}
+        steamappsFolder="$serverFolder"steamapps
+        while [[ ! -d $steamappsFolder ]]; do
+            serverFolder=${serverFolder:0:(${#serverFolder}-1)}
+            slashIndex=$(echo "$serverFolder" | grep -ob "/" | tail -n 1 | tr -d :/)
+            serverFolder=${serverFolder:0:(slashIndex + 1)}
+            steamappsFolder="$serverFolder"steamapps
+        done
         appManifest="$steamappsFolder/appmanifest"
         findAppID=$(grep "appid" "$appManifest"* | tr -d "\"")
         appID="${findAppID:8:${#findAppID}}"
-        if [[ -n $appID ]]; then 
-            updateScriptTemplate=$(cat ./update_game_template.txt)
-            updateScriptBody=${updateScriptTemplate//appid/$appID}
-            updateScriptBody=${updateScriptBody//steam-library/$serverFolder}
-            echo "$updateScriptBody" > "$updateScript"
-            steamcmd +runscript "$updateScript"
+        ### Builds path and content of SteamCMD update script based on template
+        updateScript=$serverFolder"update_${game}.txt"
+        updateScriptTemplate=$(cat ./update_game_template.txt)
+        updateScriptBody=${updateScriptTemplate//appid/$appID}
+        updateScriptBody=${updateScriptBody//steam-library/$serverFolder}
+        ### Only proceeds with creating or running script if an appID was found
+        if [[ -n $appID ]]; then
+            ### If a script isn't found or script doesn't match modified template, a new one is created
+            if [[ ! -f "$updateScript" || $(cat "$updateScript") != "$updateScriptBody" ]]; then
+                echo "$updateScriptBody" > "$updateScript"
+                echo "Creating new update script."
+            fi
+            if [[ -f "$updateScript" ]]; then
+                steamcmd +runscript "$updateScript"
+                echo "Running update script."
+            elif [[ ! -f "$updateScript" ]]; then
+                echo "Update script file not found after attempt at creating it."
+            fi
+        elif [[ -z $appID ]]; then
+            echo "No appID found."
         fi
-    elif [[ -z $steamCMDcheck ]]; then
-        echo "SteamCMD is not installed."
     fi
     case "${game}" in
         "Palworld") 
