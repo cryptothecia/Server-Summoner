ec57f4ae0ee90a0a2b41f4d146ff4d3c64b4ce9a
cryptothecia <cretaceousbob@gmail.com>
2025-10-05 23:31:25 -0400

    Added ability to run script directly to install a game by providing name and appID

--- Dedicated-Server-Scripts/Servers.sh
+++ Dedicated-Server-Scripts/Servers.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 cd "$(dirname "$0")" || exit
 source ./.PATHS
-while getopts g:v l
+while getopts g:i:v l
 do
 	case "${l}" in
 		"g") game=${OPTARG};;
 		"v") VM=true;;
+        "i") install=true; appID=${OPTARG};;
         *) game=null;;
 	esac
 done
@@ -18,13 +19,34 @@ serverLocation="${!serverLocation}"
 serverPort=_${game}_Port
 serverPort="${!serverPort}"
 
-if [[ -f $serverLocation ]]; then
+### Gets server directory and steamapps folder by searching for index of last slash, then tries to find appID
+slashIndex=$(echo "$serverLocation" | grep -ob "/" | tail -n 1 | tr -d :/)
+serverFolder=${serverLocation:0:(slashIndex + 1)}
+
+updateGame() {
+    ### $1 is $game, $2 is $appID
+    ###
+    ### Builds path and content of SteamCMD update script based on template
+    updateScript=$serverFolder"update_$1.txt"
+    updateScriptTemplate=$(cat ./update_game_template.txt)
+    updateScriptBody=${updateScriptTemplate//appid/$2}
+    updateScriptBody=${updateScriptBody//steam-library/$serverFolder}
+    echo "$updateScriptBody"
+    if [[ ! -f "$updateScript" || $(cat "$updateScript") != "$updateScriptBody" ]]; then
+        echo "$updateScriptBody" > "$updateScript"
+        echo "Creating new update script."
+    fi
+}
+
+if [[ $install == true ]]; then
+    if [[ ! -f $serverLocation ]]; then
+        mkdir "$serverLocation"
+    fi
+    updateGame "$game" "$appID"
+elif [[ -f $serverLocation && $install == false ]]; then
     if [[ $VM != true ]]; then
         ### Checks if steamcmd is a valid command
         if command -v steamcmd; then
-            ### Gets server directory and steamapps folder by searching for index of last slash, then tries to find appID
-            slashIndex=$(echo "$serverLocation" | grep -ob "/" | tail -n 1 | tr -d :/)
-            serverFolder=${serverLocation:0:(slashIndex + 1)}
             steamappsFolder="$serverFolder"steamapps
             while [[ ! -d $steamappsFolder ]]; do
                 serverFolder=${serverFolder:0:(${#serverFolder}-1)}
@@ -35,13 +57,9 @@ if [[ -f $serverLocation ]]; then
             appManifest="$steamappsFolder/appmanifest"
             findAppID=$(grep "appid" "$appManifest"* | tr -d "\"")
             appID="${findAppID:8:${#findAppID}}"
-            ### Builds path and content of SteamCMD update script based on template
-            updateScript=$serverFolder"update_${game}.txt"
-            updateScriptTemplate=$(cat ./update_game_template.txt)
-            updateScriptBody=${updateScriptTemplate//appid/$appID}
-            updateScriptBody=${updateScriptBody//steam-library/$serverFolder}
             ### Only proceeds with creating or running script if an appID was found
             if [[ -n $appID ]]; then
+                updateGame "$game" "$appID"
                 ### If a script isn't found or script doesn't match modified template, a new one is created
                 if [[ ! -f "$updateScript" || $(cat "$updateScript") != "$updateScriptBody" ]]; then
                     echo "$updateScriptBody" > "$updateScript"
@@ -69,6 +87,6 @@ if [[ -f $serverLocation ]]; then
     else
         "$serverLocation"
     fi
-else
-    echo "Server location isn't valid."
+elif [[ ! -f $serverLocation ]]; then
+    echo "Server location isn't valid. Location checked: $serverLocation"
 fi
